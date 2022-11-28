6eb230bee8d32e812c5b0befcdd4ad67bfecec90
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2022-11-28 15:04:24 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -25,6 +25,9 @@ void menu();
 void addstudent();
 void studentList();
 void addheader();
+void sortbylastname();
+void sortbydateandday();
+void sortbymonth();
 
 //MainFrame
 void addfileandheader() { 
@@ -163,9 +166,20 @@ void studentList() {
         cout << " \n\n\n";
         cout << "[1] SORT BY LASTNAME \t\t [2] SORT BY DATE AND TIME \t\t [3] SORT BY MONTH \n";
         cout << "Sort by: "; cin >> choice;
+        if(choice == 1) {
+            sortbylastname();
+        } else {
+            cout << "no such option";
+            studentList();
+        }
     }
+}
+
+
+
+void sortbylastname() {
     
-    
+
 }
 
 void getsizeandline() {
